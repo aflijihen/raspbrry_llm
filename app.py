e9e1e32878e6b3afe225be7dc8a1594a2f1f6bd5
aflijihen <99421977+aflijihen@users.noreply.github.com>
2024-09-18 01:19:32 +0200

    Update app.py

--- app.py
+++ app.py
@@ -2,8 +2,8 @@ import os
 import logging
 import time
 from dotenv import load_dotenv
-from engine_recommendation import RecommendationEngine
-from mqtt_handler import DataHandler  # Nouvelle classe qui collecte les données localement
+from recommendation_engine import RecommendationEngine
+from mqtt_handler import DataHandler  # Assurez-vous que la classe DataHandler est correctement importée
 
 load_dotenv()
 
@@ -22,15 +22,17 @@ if __name__ == "__main__":
 
     while True:
         # Collecter les données des capteurs
-        sensor_data = data_handler.collect_sensor_data()
+        sensor_data_json = data_handler.collect_sensor_data()
         
-        if sensor_data:
+        if sensor_data_json:
             # Traiter les données avec le moteur de recommandations
-            recommendation = rec_engine.generate_recommendation(sensor_data)
+            recommendation = rec_engine.generate_recommendation(sensor_data_json)
             
-            # Envoyer une notification avec les recommandations générées
-            rec_engine.notify("Recommandation Capteurs", recommendation)
+            if recommendation:
+                # Envoyer une notification avec les recommandations générées
+                rec_engine.notify("Recommandation Capteurs", recommendation)
         
         # Pause entre les cycles de collecte
         time.sleep(10)
+
 
